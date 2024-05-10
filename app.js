bb79a0e9cc183a75b6082dff29cf23114a0ccdb1
Saudha854 <157148333+Saudha854@users.noreply.github.com>
2024-05-11 01:02:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+alert ("Hello and welcome! We're thrilled to have you here");
+alert ("Error! Please enter a password");
+alert("Welcome to Js land\nHappy coding!");
+
+// Display first message
+alert("Welcome to Js land");
+
+// After a delay, display the second message
+setTimeout(function() {
+    alert("Happy coding!\nPrevent this page from creating additional dialogues");
+}, 1000); // Change 1000 to the desired delay in milliseconds (e.g., 2000 for 2 seconds)
