c91fc489e9a2d8ddd987a9ced98d830e49c3ef17
Saudha854 <157148333+Saudha854@users.noreply.github.com>
2024-05-11 18:50:32 +0500

    Add files via upload

---
+++ mathexp.js
@@ -0,0 +1,51 @@
+// Task 1 Take two numbers
+var num1 = 3;
+var num2 = 5;
+
+// Add the numbers together 
+var sum = num1 + num2;
+
+// Display the result in the browser
+document.write("<p> Sum of " + num1 + " and " + num2 + " is " + sum + ".</p>");
+
+// Task :2 Perform subtraction
+var difference = num1 - num2;
+
+// Perform multiplication
+var product = num1 * num2;
+
+// Perform division
+var quotient = num1 / num2;
+
+// Perform modulus
+var remainder = num1 % num2;
+
+// Display results in the browser
+        document.write("<p>Difference: " + difference + "</p>");
+        document.write("<p>Product: " + product + "</p>");
+        document.write("<p>Quotient: " + quotient + "</p>");
+        document.write("<p>Remainder: " + remainder + "</p>");
+
+// Task : 3 a. Declare a variable
+var myVariable = 5;
+ document.write("<p>Value after variable declaration is " + myVariable + "</p>");
+
+// Show the initial value of the variable
+document.write("<p>Initial value: " + myVariable + "</p>");
+
+//  Increment the variable
+myVariable++;
+document.write("<p>Value after increment is: " + myVariable+ "</p>");
+        
+// Add 7 to the variable
+myVariable+= 7;
+document.write("<p>Value after adding 7 is: " + myVariable + "</p>");
+
+//  Decrement the variable
+myVariable--;
+document.write("<p>Value after decrement is: " + myVariable+ "</p>");
+
+// Remainder of the value 
+var remainder = myVariable % 3;
+document.write("<p>The remainder is: " + remainder + "</p>");
+
