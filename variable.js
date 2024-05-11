e8a615eb6be199cd2ebdcfbe6bf724c2db8bb066
Saudha854 <157148333+Saudha854@users.noreply.github.com>
2024-05-11 17:11:02 +0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,19 @@
+// Task: 1 Declare 3 variables in one statement.
+var fullName, age, message;
+
+// Task :2 Declare 5 legal variables
+var firstName;
+var age;
+var _temperature;
+var $price;
+var myVar;
+// 5 Illegal variable include,2ndPlace,my-name,var,my variable;hello!
+
+// Task :3 
+document.write("<p> Variable names can only contain numbers, $, and _. For example $my_1stVariable</p>");
+document.write("<p> Variables must begin with a letter, $, or _. For example $name, _name or name</p>");
+document.write("<p> Variable names are case sensitive</p>");
+document.write("<p> Variable names should not be JS keywords</p>");
+
+
+
