c2282c943d0b342e178edb013a7d572032561c2a
Saudha854 <157148333+Saudha854@users.noreply.github.com>
2024-05-11 16:30:25 +0500

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,47 @@
+// Task : 1 Declare a variable called userName
+var userName;
+
+// Task : 2 Declare a variable & assign to it a string represents your Full Name.
+var myName = "Saudha Hira"
+
+// Task : 3 a) Declare a JS variable, titled message
+var message;
+// b) Assign “Hello World” to variable message
+var message = "Hello World";
+// c) Display the message in an alert box
+alert (message);
+
+// Task : 3  a)Save student's bio data in variables
+var studentName = "John Doe";
+var age = 14;
+var Course  = "Certified Mobile Application Development";
+
+// b)Display the data in alert boxes
+alert(studentName);
+alert(age);
+alert(Course);
+
+// Task: 4 a)Define a variable to hold the alert message
+var alertMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+// b)Display the alert using the variable
+alert(alertMessage);
+
+// Task :5 a)Declare a variable called email and assign your email.
+var email = "sudah854@gmail.com";
+
+// b)Display a message in an alert box using string concatenation
+alert("My email address is: " + email);
+
+//Task :6 Declare a variable called book & give it the value “A smarter way to learn JavaScript”.
+var book = "A smarter way to learn JavaScript";
+alert ("I am trying to learn from the book " + book);
+
+// Task : 7 a)Store the string in a variable
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// b) Display the string in an alert
+alert(specialString);
+
+// c) Display the string in the browser
+document.write("<p>" + specialString + "</p>");
